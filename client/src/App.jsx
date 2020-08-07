5826a2cc635fc189aa6d39948874a237700c404a
dtroyano86@gmail.com <dtroyano86@gmail.com>
2020-08-07 14:54:32 -0500

    bootsrap functional and map updated

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,15 +1,21 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 import MapWrapped from './components/MapWrapped.jsx';
+import Header from './components/header.jsx';
 
 const app = () => {
   return (
-    <div style={{ width: '100vw', height: '100vh' }}>
-      <MapWrapped
-        googleMapURL={`https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=geometry,drawing,places&key=${process.env.GOOGLE_MAPS_API_KEY}`}
-        containerElement={<div style={{ height: `100%` }} />}
-        mapElement={<div style={{ height: `100%` }} />}
-        loadingElement={<div style={{ height: `100%` }} />}
-      />
+    <div className='container'>
+      <Header />
+      <div className='row'>
+        <div className='col-12' style={{ width: '100%', height: '600px' }}>
+          <MapWrapped
+            googleMapURL={`https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=geometry,drawing,places&key=${process.env.GOOGLE_MAPS_API_KEY}`}
+            containerElement={<div style={{ height: `100%` }} />}
+            mapElement={<div style={{ height: `100%` }} />}
+            loadingElement={<div style={{ height: `100%` }} />}
+          />
+        </div>
+      </div>
     </div>
   );
 };
