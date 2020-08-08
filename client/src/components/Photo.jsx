8185fc21f1c2a08308e43dbe02dabe82363b8cb9
dtroyano86@gmail.com <dtroyano86@gmail.com>
2020-08-08 14:43:10 -0500

    Photo Carousel working

--- client/src/components/Photo.jsx
+++ client/src/components/Photo.jsx
@@ -1,13 +1,9 @@
 import React from 'react';
-import Comment from './Comment.jsx';
 
-const photo = ({ info: { url, comments } }) => (
-  <>
+const photo = ({ info: { url } }) => (
+  <div>
     <img className="img-thumbnail" src={url} alt="trail" />
-    {!comments
-      ? null
-      : comments.map((item) => <Comment key={item.id} text={item.text} username={item.username} />)}
-  </>
+  </div>
 );
 
 export default photo;
