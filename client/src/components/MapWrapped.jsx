7b601e95c613573418bf320b5917d35993719b59
Peter Klingelhofer <peterklingelhofer@gmail.com>
2020-08-08 19:17:35 -0500

    cleaned up code, fixed search bar location

--- client/src/components/MapWrapped.jsx
+++ client/src/components/MapWrapped.jsx
@@ -36,14 +36,6 @@ const MapWithASearchBox = compose(
         isOpen: !isOpen,
       }),
     },
-    () => ({
-      selectedPlace: null,
-    }),
-    {
-      onToggleOpen: (i) => () => ({
-        selectedPlace: i,
-      }),
-    },
   ),
   withProps({
     googleMapURL: `https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=geometry,drawing,places&key=${process.env.GOOGLE_MAPS_API_KEY}`,
@@ -52,6 +44,11 @@ const MapWithASearchBox = compose(
     mapElement: <div style={{ height: '100%' }} />,
   }),
   lifecycle({
+    selectPlace(i) {
+      this.setState({
+        selectedPlace: i,
+      });
+    },
     componentWillMount() {
       const refs = {};
 
@@ -169,76 +166,3 @@ const MapWithASearchBox = compose(
 ));
 
 export default MapWithASearchBox;
-
-// const MapWrapped = withScriptjs(withGoogleMap(MapWithASearchBox));
-// export default MapWrapped;
-
-// import React, { useState, useEffect } from 'react';
-// import {
-//   GoogleMap,
-//   withGoogleMap,
-//   withScriptjs,
-//   Marker,
-//   InfoWindow,
-// } from 'react-google-maps';
-
-// import * as trailData from '../data/trail-data.json';
-
-// function Map() {
-//   const [selectedTrail, setSelectedTrail] = useState(null);
-//   const [userLocation, setUserLocation] = useState({ lat: 30.33735, lng: -90.03733 })
-
-//   useEffect(() => {
-//     const listener = (e) => {
-//       if (e.key === 'Escape') {
-//         setSelectedTrail(null);
-//       }
-//     };
-//     window.addEventListener('keydown', listener);
-//     navigator.geolocation.getCurrentPosition((position) => {
-//       const { latitude, longitude } = position.coords;
-//       setUserLocation({ lat: latitude, lng: longitude });
-//     });
-//     return () => {
-//       window.removeEventListener('keydown', listener);
-//     };
-//   }, []);
-
-//   return (
-//     <GoogleMap defaultZoom={12} defaultCenter={userLocation} center={userLocation}>
-//       {trailData.data.map((trail) => (
-//         <Marker
-//           key={trail.id}
-//           position={{
-//             lat: +trail.lat,
-//             lng: +trail.lon,
-//           }}
-//           onClick={() => {
-//             setSelectedTrail(trail);
-//           }}
-//         />
-//       ))}
-
-//       {selectedTrail && (
-//         <InfoWindow
-//           onCloseClick={() => {
-//             setSelectedTrail(null);
-//           }}
-//           position={{
-//             lat: +selectedTrail.lat,
-//             lng: +selectedTrail.lon,
-//           }}
-//         >
-//           <div>
-//             <h2>{selectedTrail.name}</h2>
-//             <p>{selectedTrail.description}</p>
-//           </div>
-//         </InfoWindow>
-//       )}
-//     </GoogleMap>
-//   );
-// }
-
-// const MapWrapped = withScriptjs(withGoogleMap(Map));
-
-// export default MapWrapped;
