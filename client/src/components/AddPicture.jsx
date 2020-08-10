539c61f40c20a8ff1a5f205ef2cdac0914368eb9
dtroyano86@gmail.com <dtroyano86@gmail.com>
2020-08-10 12:24:05 -0500

    Modal Working

--- client/src/components/AddPicture.jsx
+++ client/src/components/AddPicture.jsx
@@ -1,8 +1,37 @@
-import React from 'react';
+import React, { useState } from 'react';
+import Button from 'react-bootstrap/Button';
+import Modal from 'react-bootstrap/Modal';
+import Image from 'react-bootstrap/Image';
+import Input from './input.jsx';
+import useForm from '../helpers';
 
 const addPicture = () => {
+  const [show, setShow] = useState(false);
+  const toggleModal = () => setShow(!show);
+  const submitPicture = (data) => {
+    toggleModal();
+    console.log(data);
+  };
+  const { values, changeHandler, submitHandler } = useForm(submitPicture);
+
+
   return (
-    <button type="button" class="btn btn-success">Add Picture</button>
+    <>
+      <Button variant="success" onClick={toggleModal}>Add Picture</Button>
+      <Modal show={show} onHide={toggleModal}>
+        <Modal.Header closeButton>
+          <Modal.Title>Add New Picture</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <Input value={values.url} name="url" label="Url:" changeHandler={changeHandler} />
+          <Image src={values.url} thumbnail />
+        </Modal.Body>
+        <Modal.Footer>
+          <Button variant="danger" onClick={toggleModal}>Close</Button>
+          <Button variant="success" onClick={submitHandler}>Submit</Button>
+        </Modal.Footer>
+      </Modal>
+    </>
   );
 };
 
