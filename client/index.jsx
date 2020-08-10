f33ce716e4f20e7a8cbcf5cfcf3bfc702a24a6f5
dtroyano86@gmail.com <dtroyano86@gmail.com>
2020-08-10 11:53:18 -0500

    Swapped over to react-Bootstrap

--- client/index.jsx
+++ client/index.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-
+import 'bootstrap/dist/css/bootstrap.min.css';
 import App from './src/App.jsx';
 
 ReactDOM.render(<App />, document.getElementById('app'));
