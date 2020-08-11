4aaf7d71dee4062215ead0d47b4ccfdb5ffe9e6f
dtroyano86@gmail.com <dtroyano86@gmail.com>
2020-08-11 13:44:57 -0500

    header fixed

--- client/src/components/header.jsx
+++ client/src/components/header.jsx
@@ -1,30 +1,23 @@
 import React from 'react';
+import Navbar from 'react-bootstrap/Navbar';
 import { Link } from 'react-router-dom';
 
 const header = () => (
-  <nav className="navbar navbar-expand-lg navbar-light bg-light">
+  <Navbar expand="lg" bg="light">
     <Link to="/" className="navbar-brand">Trailr</Link>
-    <button
-      className="navbar-toggler"
-      type="button"
-      data-toggle="collapse"
-      data-target="#navbarNavAltMarkup"
-      aria-controls="navbarNavAltMarkup"
-      aria-expanded="false"
-      aria-label="Toggle navigation"
-    >
+    <Navbar.Toggle aria-controls="menuBar">
       <span className="navbar-toggler-icon" />
-    </button>
-    <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
+    </Navbar.Toggle>
+    <Navbar.Collapse id="menuBar">
       <div className="navbar-nav">
-        <Link to="/user/0" className="nav-item nav-link active">User</Link>
-        <Link to="/trail/1" className="nav-item nav-link active">Trail</Link>
+        <Link to="/user/0" className="nav-item nav-link">User</Link>
+        <Link to="/trail/1" className="nav-item nav-link">Trail</Link>
         <a href="/auth/login" className="nav-item nav-link">Login</a>
         <a href="/auth/logout" className="nav-item nav-link">Signout</a>
         <a href="/auth/google" className="nav-item nav-link">Google</a>
       </div>
-    </div>
-  </nav>
+    </Navbar.Collapse>
+  </Navbar>
 );
 
 export default header;
