6ddcb7caebc69159f7fffc273fea4be2a651724b
dtroyano86@gmail.com <dtroyano86@gmail.com>
2020-08-11 15:44:04 -0500

    Add Comment Documented

--- client/src/components/AddComment.jsx
+++ client/src/components/AddComment.jsx
@@ -1,8 +1,18 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import useForm from '../helpers';
 import Input from './input.jsx';
 
+/**
+ * Displays an Input textarea and a submit button for adding a comment, upon
+ *  submit it runs the appendComments function and updates the database
+ * @param {Function} appendComments Function that appends comments to the photo
+ */
 const addComment = ({ appendComments }) => {
+  /**
+   * Runs on the form submit and updates database and runs appendComments
+   * @param {String} text extracted from a values object
+   */
   const commentSubmit = ({ text }) => {
     console.log(text);
     appendComments({ text, id: text[0], username: 'Danny' });
@@ -19,3 +29,7 @@ const addComment = ({ appendComments }) => {
 };
 
 export default addComment;
+
+addComment.propTypes = {
+  appendComments: PropTypes.func.isRequired,
+};
