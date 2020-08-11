749262d052dbba953125950f3905bae3e2d4a172
Peter Klingelhofer <peterklingelhofer@gmail.com>
2020-08-11 11:53:33 -0500

    working on bootstrapping infowindow

--- client/src/components/InfoWindow.jsx
+++ client/src/components/InfoWindow.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { Overlay, OverlayTrigger, Popover, Button } from 'react-bootstrap';
 
 const InfoWindow = ({ props, onCloseClick }) => {
   const { selectedTrail } = props;
@@ -17,24 +18,77 @@ const InfoWindow = ({ props, onCloseClick }) => {
   };
 
   return (
-    <div style={infoWindowStyle}>
-      <div style={{ fontSize: 12, position: 'relative', left: '96%' }} onClick={onCloseClick}>x</div>
-      <Link to={`/trail/${place.id}`} activeclassname="active">
-        <h2>{place.name}</h2>
-      </Link>
-      <div style={{ fontSize: 14 }}>
-        <span style={{ color: 'grey' }}>{place.rating} </span>
-        <span style={{ color: 'orange' }}>
-          {String.fromCharCode(9733).repeat(Math.floor(place.rating))}
-        </span>
-        <span style={{ color: 'lightgrey' }}>
-          {String.fromCharCode(9733).repeat(5 - Math.floor(place.rating))}
-        </span>
-      </div>
-      <div style={{ fontSize: 14, color: 'grey' }}>{place.length} miles</div>
-      <div style={{ fontSize: 14, color: 'green' }}>{place.description}</div>
-    </div>
+    <>
+      {['left'].map((placement) => (
+        <OverlayTrigger
+          trigger="click"
+          key={placement}
+          placement={placement}
+          overlay={<Popover id={`popover-positioned-${placement}`} />}
+        >
+          <Button variant="secondary">
+            <div
+              style={{
+                fontSize: 12,
+                position: 'relative',
+                left: '50%',
+                color: 'black',
+              }}
+              onClick={onCloseClick}
+            >
+              x
+            </div>
+            <Link to={`/trail/${place.id}`} activeclassname="active">
+              <h2>{place.name}</h2>
+            </Link>
+            <div style={{ fontSize: 14 }}>
+              <span style={{ color: 'black' }}>{place.rating} </span>
+              <span style={{ color: 'orange' }}>
+                {String.fromCharCode(9733).repeat(Math.floor(place.rating))}
+              </span>
+              <span style={{ color: 'grey' }}>
+                {String.fromCharCode(9733).repeat(5 - Math.floor(place.rating))}
+              </span>
+            </div>
+            <div style={{ fontSize: 14, color: 'black' }}>
+              {place.length} miles
+            </div>
+            <div
+              className="text-truncate"
+              style={{ fontSize: 14, color: 'black', width: '20rem' }}
+            >
+              {place.description}
+            </div>
+          </Button>
+        </OverlayTrigger>
+      ))}
+    </>
   );
 };
 
 export default InfoWindow;
+
+{
+  /* <div style={infoWindowStyle}>
+<div
+  style={{ fontSize: 12, position: 'relative', left: '96%' }}
+  onClick={onCloseClick}
+>
+  x
+</div>
+<Link to={`/trail/${place.id}`} activeclassname="active">
+  <h2>{place.name}</h2>
+</Link>
+<div style={{ fontSize: 14 }}>
+  <span style={{ color: 'grey' }}>{place.rating} </span>
+  <span style={{ color: 'orange' }}>
+    {String.fromCharCode(9733).repeat(Math.floor(place.rating))}
+  </span>
+  <span style={{ color: 'lightgrey' }}>
+    {String.fromCharCode(9733).repeat(5 - Math.floor(place.rating))}
+  </span>
+</div>
+<div style={{ fontSize: 14, color: 'grey' }}>{place.length} miles</div>
+<div style={{ fontSize: 14, color: 'green' }}>{place.description}</div>
+</div> */
+}
