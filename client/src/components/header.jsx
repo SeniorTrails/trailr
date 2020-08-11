91baa242b0c8058c2f0dc0f8594fa8ff0a300739
dtroyano86@gmail.com <dtroyano86@gmail.com>
2020-08-11 13:50:47 -0500

    touched it up a little more

--- client/src/components/header.jsx
+++ client/src/components/header.jsx
@@ -1,21 +1,20 @@
 import React from 'react';
 import Navbar from 'react-bootstrap/Navbar';
+import Nav from 'react-bootstrap/Nav';
 import { Link } from 'react-router-dom';
 
 const header = () => (
   <Navbar expand="lg" bg="light">
     <Link to="/" className="navbar-brand">Trailr</Link>
-    <Navbar.Toggle aria-controls="menuBar">
-      <span className="navbar-toggler-icon" />
-    </Navbar.Toggle>
+    <Navbar.Toggle aria-controls="menuBar" />
     <Navbar.Collapse id="menuBar">
-      <div className="navbar-nav">
+      <Nav>
         <Link to="/user/0" className="nav-item nav-link">User</Link>
         <Link to="/trail/1" className="nav-item nav-link">Trail</Link>
-        <a href="/auth/login" className="nav-item nav-link">Login</a>
-        <a href="/auth/logout" className="nav-item nav-link">Signout</a>
-        <a href="/auth/google" className="nav-item nav-link">Google</a>
-      </div>
+        <Nav.Link href="/auth/login">Login</Nav.Link>
+        <Nav.Link href="/auth/logout">Signout</Nav.Link>
+        <Nav.Link href="/auth/google">Google</Nav.Link>
+      </Nav>
     </Navbar.Collapse>
   </Navbar>
 );
