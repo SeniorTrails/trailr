06f3efa28bbb0f27eb6ca8bb83de6215c120cfee
dtroyano86@gmail.com <dtroyano86@gmail.com>
2020-08-12 14:13:23 -0500

    Input documentation

--- client/src/components/input.jsx
+++ client/src/components/input.jsx
@@ -5,32 +5,43 @@ import PropTypes from 'prop-types';
  * input: An input component that displays a label and an input field
  *  type will default to 'text' but if it is 'select' you must provide
  *  an options array that is made of objects with a value and a label
- * @param {Object} props value, changeHandler, name, label, type, options
- * @returns {JSX} label and input
+ * @param {(Number|String)} value value for the input component
+ * @param {Function} changeHandler function that handles changes
+ * @param {String} name unique name of the value
+ * @param {String} label label to be displayed
+ * @param {String} type defaults to text, type of input component
+ * @param {Array} options optional, for a select component array of {value, label}
  */
-const input = ({ value = '', changeHandler, name, label, type = 'text', options, style }) => {
+const input = ({
+  value = '',
+  changeHandler,
+  name,
+  label,
+  type = 'text',
+  options,
+  style,
+}) => {
   let component = null;
 
   switch (type) {
     case 'select':
       component = (
-        <select onChange={changeHandler} name={name} value={value}>
+        <select id={name} onChange={changeHandler} name={name} value={value}>
           {options.map((item) => <option key={item.value} value={item.value}>{item.label}</option>)}
         </select>
       );
       break;
     case 'textarea':
-      component = <textarea name={name} value={value} onChange={changeHandler} rows="4" cols="30" />;
+      component = <textarea id={name} name={name} value={value} onChange={changeHandler} rows="4" cols="30" />;
       break;
     case 'text':
     default:
-      component = <input name={name} type="text" value={value} onChange={changeHandler} />;
+      component = <input id={name} name={name} type="text" value={value} onChange={changeHandler} />;
   }
 
   return (
     <div style={style}>
-      <label>
-        {label}
+      <label htmlFor={name}>{label}
         {component}
       </label>
     </div>
@@ -40,5 +51,23 @@ const input = ({ value = '', changeHandler, name, label, type = 'text', options,
 export default input;
 
 input.propTypes = {
-
+  value: PropTypes.oneOfType([
+    PropTypes.string,
+    PropTypes.number,
+  ]),
+  changeHandler: PropTypes.func.isRequired,
+  name: PropTypes.string.isRequired,
+  label: PropTypes.string,
+  type: PropTypes.string,
+  options: PropTypes.arrayOf(
+    PropTypes.shape({
+      value: PropTypes.oneOf([
+        PropTypes.number,
+        PropTypes.string,
+      ]),
+      label: PropTypes.string,
+    }),
+  ),
+  // eslint-disable-next-line react/forbid-prop-types
+  style: PropTypes.object,
 };
