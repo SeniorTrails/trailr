b36a787dbbcd3ee76abc2a5882767cec94823239
dtroyano86@gmail.com <dtroyano86@gmail.com>
2020-08-12 10:20:48 -0500

    GoogleMap documentation

--- client/src/components/GoogleMap.jsx
+++ client/src/components/GoogleMap.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import styled from 'styled-components';
 import GoogleMapReact from 'google-map-react';
 
@@ -7,12 +8,16 @@ const Wrapper = styled.main`
   height: 100%;
 `;
 
+/**
+ * Wraps the Google Map in a 100% size wrapper and returns it
+ */
 const GoogleMap = ({ children, ...props }) => (
   <Wrapper>
     <GoogleMapReact
       bootstrapURLKeys={{
         key: process.env.REACT_APP_MAP_KEY,
       }}
+      // eslint-disable-next-line react/jsx-props-no-spreading
       {...props}
     >
       {children}
@@ -20,9 +25,12 @@ const GoogleMap = ({ children, ...props }) => (
   </Wrapper>
 );
 
-
 GoogleMap.defaultProps = {
   children: null,
 };
 
 export default GoogleMap;
+
+GoogleMap.propTypes = {
+  children: PropTypes.node,
+};
