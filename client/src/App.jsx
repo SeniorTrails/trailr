cd0188ee6628675ce4dd272ca36b7939638acd4c
Peter Klingelhofer <peterklingelhofer@gmail.com>
2020-08-13 11:03:19 -0500

    header updates

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,39 +1,16 @@
 import React from 'react';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
-import { Root, Routes } from 'react-static';
 import MapWrapped from './components/MapWrapped.jsx';
 import Header from './components/header.jsx';
 import Trail from './components/Trail.jsx';
 import User from './components/User.jsx';
 import Signup from './components/Signup.jsx';
 import Login from './components/Login.jsx';
-import mountainHeaderImage from '../assets/imgs/mountainHeader.png';
+import HeaderImage from './components/HeaderImage.jsx';
 
 const app = () => (
   <BrowserRouter>
-    <div>
-      <div style={{ position: 'absolute', left: '50%' }}>
-        <div
-          style={{
-            position: 'relative',
-            left: '-50%',
-          }}
-        >
-          <img
-            src={mountainHeaderImage}
-            style={{ width: '380px' }}
-            className="img-fluid"
-            alt="Mountain trail"
-          />
-        </div>
-      </div>
-    </div>
-    <br />
-    <br />
-    <br />
-    <br />
-    <br />
-    <br />
+    <HeaderImage />
     <div className="container">
       <Header />
       <div className="row">
@@ -54,7 +31,7 @@ const app = () => (
             <div className="col-12" style={{ width: '100%', height: '600px' }}>
               <MapWrapped
                 googleMapURL={`https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=geometry,drawing,places&key=${process.env.GOOGLE_MAPS_API_KEY}`}
-                containerElement={<div style={{ height: '1000px' }} />}
+                containerElement={<div style={{ height: '800px' }} />}
                 mapElement={<div style={{ height: '100%' }} />}
                 loadingElement={<div style={{ height: '100%' }} />}
               />
