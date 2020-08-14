c68170186a2b0653932c3ff56a5caf79f44a6939
dtroyano86@gmail.com <dtroyano86@gmail.com>
2020-08-14 18:41:12 -0500

    User now checks correctly

--- client/src/components/User.jsx
+++ client/src/components/User.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { Link, useParams } from 'react-router-dom';
+import { Link, useParams, Redirect } from 'react-router-dom';
 import PropTypes from 'prop-types';
 import Col from 'react-bootstrap/Col';
 import Row from 'react-bootstrap/Row';
@@ -7,7 +7,7 @@ import Accordion from 'react-bootstrap/Accordion';
 import Card from 'react-bootstrap/Card';
 import Badge from 'react-bootstrap/Badge';
 import Image from 'react-bootstrap/Image';
-import { getUserData } from '../helpers';
+import { getUserData, getAuth } from '../helpers';
 import Carousel from './Carousel.jsx';
 import AddComment from './AddComment.jsx';
 
@@ -20,16 +20,36 @@ const userPage = ({ user }) => {
   const [photoInfo, setPhotoInfo] = useState([]);
   const [currentPhoto, setCurrentPhoto] = useState(0);
   const [myTrails, setMyTrails] = useState([]);
+  const [redirect, setRedirect] = useState(false);
+  const [userInfo, setUserInfo] = useState({});
 
   // Set all the initial data with DB calls based on id in useParams
+  // You can only see your user page
   useEffect(() => {
-    getUserData(id)
-      .then((userData) => {
-        setPhotoInfo(userData.photos);
-        setMyTrails(userData.favorites);
+    getAuth()
+      .then((response) => {
+        if (id == response.user.id) {
+          getUserData(id)
+            .then((userData) => {
+              if (Array.isArray(userData)) {
+                setRedirect(true);
+              } else {
+                setPhotoInfo(userData.photos);
+                setMyTrails(userData.favorites);
+                setUserInfo({ name: userData.name, url: userData.profile_photo_url });
+              }
+            })
+            .catch((err) => {
+              console.error(err);
+              setRedirect(true);
+            });
+        } else {
+          setRedirect(true);
+        }
       })
       .catch((err) => {
         console.error(err);
+        setRedirect(true);
       });
   }, []);
 
@@ -56,6 +76,7 @@ const userPage = ({ user }) => {
 
   return (
     <>
+      {redirect ? <Redirect to='/404' /> : null}
       <Col xs={6}>
         {!photoInfo.length
           ? null
@@ -81,7 +102,8 @@ const userPage = ({ user }) => {
           )}
       </Col>
       <Col xs={6}>
-        <h2>Saved Trails</h2>
+        <Image thumbnail src={userInfo.url} />
+        <h2>{userInfo.name}'s Saved Trails</h2>
         {!myTrails.length
           ? null
           : myTrails.map((trail) => (
